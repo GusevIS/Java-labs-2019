89c4e1e763f06e8884b037d4853bbe2688bc643f
GusevIS <39344869+GusevIS@users.noreply.github.com>
2019-04-18 22:38:08 +0300

    fixed

--- Lab3/Stock.java
+++ Lab3/Stock.java
@@ -1,53 +1,12 @@
 public class Stock
 {
     double count = 0;
-    int turn;
+    int countOfThreads = 0;
 
-    Stock(int parameter1, int parameter2)
+    synchronized void add(double num)
     {
-        if ((parameter1 == 0) || (parameter2 == 0))
-        {
-            turn = 0;
-        }
-        else
-            turn = 1;
-    }
-
-    synchronized void add(double num, int queueIsNotActive)
-    {
-        if ((turn == 1) &&  (Thread.currentThread().getName() == "SecondStream"))
-        {
-            try
-            {
-                wait();
-            }
-            catch (InterruptedException e)
-            {}
-        }
-        else if ((turn == 2) &&  (Thread.currentThread().getName() == "FirstStream"))
-        {
-            try
-            {
-                wait();
-            }
-            catch (InterruptedException e)
-            {}
-        }
-
         this.count += num;
         System.out.println("add " + num + " " + Thread.currentThread().getName());
         System.out.println("current stock " + count + "\n");
-
-        if (queueIsNotActive == 1)
-        {
-            turn = 0;
-        }
-
-        if (turn == 1)
-            turn = 2;
-        else if (turn == 2)
-            turn = 1;
-
-        notify();
     }
 }
